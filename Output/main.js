35fbc2f9880270a5e67e48a11ec96468d98e4f08
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2021-11-22 11:54:49 +0600

    output

--- Output/main.js
+++ Output/main.js
@@ -1,1 +1,21 @@
-console.log("Mahbub");
+// innerHTML
+document.getElementById("innerHTML").innerHTML = "I am from innerHTML";
+
+// document.write()
+var test = "I am from document.write()";
+document.write(test); //only number & var  can read
+
+// window.alert()
+window.alert("I am from window.alert()");
+
+// console.log()
+console.log("I am from console.log");
+
+
+// we can ignore window part
+// write(test);
+// alert("I am from window.alert()");
+
+
+
+
